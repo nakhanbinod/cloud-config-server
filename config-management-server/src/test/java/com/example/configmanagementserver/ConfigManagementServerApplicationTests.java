1cde75f3a072f00177fb6c7bca3151878aead5e9
Binod Nakhan <binodnakhan@Binods-MacBook-Pro.local>
2025-09-02 21:08:17 +0545

    added config and demo project where we use whole configuration to a vault and access v1 config

---
+++ config-management-server/src/test/java/com/example/configmanagementserver/ConfigManagementServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.configmanagementserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigManagementServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
